835ac8e890a67b098cf992d550e45815b8dad9f5
Hugo Catarino <hugocatarino1@gmail.com>
2019-09-21 17:11:42 -0300

    Start Project IG

---
+++ Person.py
@@ -0,0 +1,17 @@
+class Person():
+    def__init__(self, first_name, last_name,
+                email)
+    self.first_name = first_name
+    self.last_name = last_name
+    self.email = email
+
+
+
+class Teste():
+    first_name = 'Hugo'
+    last_name = 'Catarino'
+    email = 'email'
+    Person(first_name,last_name,email)
+
+
+Teste().run()
